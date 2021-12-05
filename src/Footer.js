e45812e2bdb1d6556f7b4c329a371819d0156b38
amy-calleja <91883080+amy-calleja@users.noreply.github.com>
2021-12-05 16:32:46 +0100

    created/imported Footer component

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <a
+        href="https://github.com/amy-calleja/react-weather-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open-source
+      </a>
+      <p> React app created by Amy Calleja 👩‍💻</p>
+    </div>
+  );
+}
