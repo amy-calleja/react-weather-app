533ad3d4526bf9d88cca27fe5356e56def712842
amy-calleja <91883080+amy-calleja@users.noreply.github.com>
2021-12-05 16:16:16 +0100

    updated className

--- src/Weather.js
+++ src/Weather.js
@@ -3,8 +3,8 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div class="Weather">
-      <form class="search-bar" id="search-form">
+    <div className="Weather">
+      <form className="search-bar" id="search-form">
         <input
           type="text"
           autocomplete="off"
@@ -14,27 +14,27 @@ export default function Weather() {
         <span />
         <input type="submit" value="Go" id="go" />
         <button id="locationButton">
-          <i class="fas fa-thumbtack"></i>My Location
+          <i className="fas fa-thumbtack"></i>My Location
         </button>
       </form>
 
       <h2>Sydney</h2>
       <p>10th November 2021</p>
-      <div class="temperatures-display">
+      <div className="temperatures-display">
         <img
           src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
           alt="Partly Cloudy"
         />
         <h1>20</h1>
-        <span class="units">°C</span>
+        <span className="units">°C</span>
       </div>
 
       <h3>Clear Sky</h3>
-      <div class="humidityWind">
+      <div className="humidityWind">
         <div>Humidity: 80%</div>
         <div>Wind: 5km/h</div>
       </div>
-      <div class="weather-forecast" id="forecast"></div>
+      <div className="weather-forecast" id="forecast"></div>
     </div>
   );
 }
